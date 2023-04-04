e0e6ed9b116a3b9a7d21b0541e438eafd99f4448
= <limalorruama98@gmail.com>
2023-04-04 13:22:48 -0300

    menu

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,21 @@
+import styles from './PaginaInicial.module.scss'
+import banner from './banner.png'
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
 
-export default function PaginaInicial(){
-    return(
-        <Cabecalho />
-    )
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa de fotos do espaço!</h1>
+            <img  src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
