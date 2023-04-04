d09d2f0900fd5c19b851455d5cc6574109bfd8be
= <limalorruama98@gmail.com>
2023-04-04 13:39:53 -0300

    alterando fonte

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./assets/fontes/GandhiSans-Regular.otf"
+import "./assets/fontes/GandhiSans-Bold.otf"
 
 //https://www.figma.com/file/Y1W8HJHKqlUdDFeWi8e4cz/Alura-Space-%7C-React%3A-arquivos-est%C3%A1ticos?node-id=89%3A4
 
